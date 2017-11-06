608955d0ce9151deda879d099c5ee347146da800
Sean Derrow <sderrow@mit.edu>
2017-11-06 14:54:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,25 @@ function addElementToEndOfArray(my_array, my_element) {
 function destructivelyAddElementToEndOfArray(my_array, my_element) {
   return my_array.push(my_element)
 }
+
+function accessElementInArray(my_array, my_index) {
+  return my_array[my_index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(my_array) {
+  my_array.shift()
+  return my_array
+}
+
+function removeElementFromBeginningOfArray(my_array) {
+  return my_array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(my_array) {
+  my_array.pop()
+  return my_array
+}
+
+function removeElementFromEndOfArray(my_array) {
+  return my_array.slice(0, my_array.length - 1)
+}
